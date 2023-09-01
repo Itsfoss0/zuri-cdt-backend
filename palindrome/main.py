b8cdbb28e1462e85287016fd4afa6d319022d276
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-01 13:31:06 +0300

    [Chore]: Example usage

--- palindrome/main.py
+++ palindrome/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
-Test script
+Test script with examples of how to use
+CasCading Palindrome
 """
 
 from palindromes import CascadingPalindrome
@@ -11,3 +12,17 @@ print(cascading)
 
 cascading = CascadingPalindrome('hi mom')
 print(cascading)
+
+multiple_match = CascadingPalindrome('mom dad hello world bob lee')
+print(multiple_match)
+
+#catching the error thrown
+try:
+    palindrome = CascadingPalindrome(1337)
+    print(palindrome)
+except TypeError:
+    print('An exception was thrown')
+
+
+palindrome = 'hello world'
+print(CascadingPalindrome(palindrome))
