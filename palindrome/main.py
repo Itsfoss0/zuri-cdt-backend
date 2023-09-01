b9648034c62dd917b72122682b22edbfaf6adf15
Ebenezer <akhonyaebenezer@gmail.com>
2023-09-01 13:16:48 +0300

    [Chore]: Main file

---
+++ palindrome/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Test script
+"""
+
+from palindromes import CascadingPalindrome
+
+cascading = CascadingPalindrome('hello world')
+print(cascading)
+
+cascading = CascadingPalindrome('hi mom')
+print(cascading)
